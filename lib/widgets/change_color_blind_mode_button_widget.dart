f573502599c6a9f2c118c01b92f0208f198a1802
Poyraz Aktas <poyraz.educational@gmail.com>
2022-01-31 17:40:55 +0300

    add circular progress widget to simulate loading state

--- lib/widgets/change_color_blind_mode_button_widget.dart
+++ lib/widgets/change_color_blind_mode_button_widget.dart
@@ -4,8 +4,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wordle_turkce/provider/color_blind_mode_provider.dart';
 
 class ChangeColorBlindModeButtonWidget extends StatefulWidget {
-  const ChangeColorBlindModeButtonWidget({Key? key}) : super(key: key);
-
   @override
   State<ChangeColorBlindModeButtonWidget> createState() =>
       _ChangeColorBlindModeButtonWidgetState();
@@ -15,9 +13,13 @@ class _ChangeColorBlindModeButtonWidgetState
     extends State<ChangeColorBlindModeButtonWidget> {
   late SharedPreferences _prefs;
   late bool isColorBlindModeActive;
+  bool isLoading = true;
 
   void _initSharedPrefs() async {
     _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -28,11 +30,15 @@ class _ChangeColorBlindModeButtonWidgetState
 
   @override
   Widget build(BuildContext context) {
+    final colorBlindModeProvider = Provider.of<ColorBlindModeProvider>(context);
+    if (isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
 
     return Switch.adaptive(
         value: _prefs.getBool("isColorBlindModeActive")!,
         activeColor: Colors.lightGreenAccent.shade700,
-        onChanged: (value) async{
+        onChanged: (value) async {
           final provider =
               Provider.of<ColorBlindModeProvider>(context, listen: false);
           await provider.toggleMode(value);
